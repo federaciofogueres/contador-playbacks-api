50593c1106952917e7f6b21bc1a0552b3da10db9
Fran García <fgarciadevelop@gmail.com>
2023-11-03 13:31:05 +0100

    fix: fix 200 response

--- service/SettingsService.js
+++ service/SettingsService.js
@@ -6,11 +6,22 @@
  * Sends an email from the system
  *
  * body EmailItem Data to send (optional)
- * no response value expected for this operation
+ * returns inline_response_200
  **/
 exports.sendEmail = function(body) {
   return new Promise(function(resolve, reject) {
-    resolve();
+    var examples = {};
+    examples['application/json'] = {
+  "status" : {
+    "message" : "Example message",
+    "status" : "200"
+  }
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
   });
 }
 
