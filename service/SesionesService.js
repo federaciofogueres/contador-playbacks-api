faf2ebd20ad31e5fe9c54852ba27f48138a34559
Fran García <fgarciadevelop@gmail.com>
2024-11-23 13:50:20 +0100

    feat: changes on post session

--- service/SesionesService.js
+++ service/SesionesService.js
@@ -6,8 +6,8 @@ var extraService = require("../service/ExtraService");
  *
  * returns SessionsResponse
  **/
-exports.getAllSessions = function() {
-  return new Promise(function(resolve, reject) {
+exports.getAllSessions = function () {
+  return new Promise(function (resolve, reject) {
     extraService.get(null, 'session').then(res => {
       resolve(extraService.transformResponse(res.filter(r => r.active === 1), 'sessions'));
     }).catch(res => {
@@ -23,8 +23,8 @@ exports.getAllSessions = function() {
  * idSession String 
  * returns SessionResponse
  **/
-exports.getSession = function(idSession) {
-  return new Promise(async function (resolve, reject){
+exports.getSession = function (idSession) {
+  return new Promise(async function (resolve, reject) {
     try {
       let session = await extraService.get(idSession, 'session');
       let asociaciones = await extraService.get(idSession, 'asociacion_session');
@@ -32,23 +32,23 @@ exports.getSession = function(idSession) {
       let response = session[0];
       response['participants'] = [];
       if (asociaciones !== 0) {
-        for(let asociacion of asociaciones) {
+        for (let asociacion of asociaciones) {
           let asociacionesInfo = await extraService.get(asociacion.id_asociacion, 'asociacion');
           response.participants.push(asociacionesInfo[0]);
         }
       }
       response['type_normalized'] = type[0].type_normalized;
       resolve(extraService.transformResponse(response, 'session'));
-    } catch(error) {
+    } catch (error) {
       reject(extraService.transformResponse(error, null, false));
     }
   })
 }
 
-var createRelation = async function(id_asociacion, id_session) {
-  return new Promise(async function(resolve, reject) {
+var createRelation = async function (id_asociacion, id_session) {
+  return new Promise(async function (resolve, reject) {
     try {
-      let postRelation =  {
+      let postRelation = {
         id_asociacion: id_asociacion,
         id_session: id_session
       }
@@ -66,26 +66,26 @@ var createRelation = async function(id_asociacion, id_session) {
  * body Session  (optional)
  * returns inline_response_200
  **/
-exports.createSesion = function(body) {
-  return new Promise(async function (resolve, reject){
+exports.createSesion = function (body) {
+  return new Promise(async function (resolve, reject) {
     let postSession = {
       id: body.id,
       session_title: body.session_title,
       type: body.type
     }
     try {
-      await extraService.set(postSession, 'session'); // Returns 1 if OK
-      for(let asociacion of body.participants) {
-        createRelation(asociacion.id, body.id);
+      let resultId = await extraService.set(postSession, 'session', true); // Returns 1 if OK
+      for (let asociacion of body.participants) {
+        createRelation(asociacion.id, resultId);
       }
       resolve(extraService.transformResponse(null, null, true));
-    } catch(error) {
+    } catch (error) {
       reject(extraService.transformResponse(error, null, false));
     }
   })
 }
 
-var removeRelations = function() {
+var removeRelations = function () {
 
 }
 
@@ -95,15 +95,15 @@ var removeRelations = function() {
  * idSession String 
  * returns inline_response_200_1
  **/
-exports.deleteSesion = function(idSession) {
-  return new Promise(async function (resolve, reject){
+exports.deleteSesion = function (idSession) {
+  return new Promise(async function (resolve, reject) {
     try {
       if (await extraService.get(idSession, 'asociacion_session') !== 0) {
         await extraService.delete(idSession, 'asociacion_session');
       }
       await extraService.delete(idSession, 'session')
       resolve(extraService.transformResponse(null, null, true));
-    } catch(error) {
+    } catch (error) {
       reject(extraService.transformResponse(error, null, false));
     }
   })
@@ -116,28 +116,28 @@ exports.deleteSesion = function(idSession) {
  * idSession String 
  * returns inline_response_200_1
  **/
-exports.putSesion = function(body, idSession) {
-  return new Promise(async function(resolve, reject) {
+exports.putSesion = function (body, idSession) {
+  return new Promise(async function (resolve, reject) {
     try {
       await updateRelation(body, idSession);
       delete body.type_normalized;
       delete body.participants;
       await extraService.update(body, 'session', idSession);
       reject(extraService.transformResponse(null, null, true));
-    } catch(error) {
+    } catch (error) {
       reject(extraService.transformResponse(error, null, false));
     }
   });
 }
 
-var updateRelation = async function(body, idSession) {
-  return new Promise(async function(resolve,reject) {
+var updateRelation = async function (body, idSession) {
+  return new Promise(async function (resolve, reject) {
 
     let newAsociacionesOnSession = [];
     body.participants.map(asociacion => {
       newAsociacionesOnSession.push(asociacion.id)
     })
-    let oldAsociacionesOnSession =  [];
+    let oldAsociacionesOnSession = [];
     let asociaciones = await extraService.get(idSession, 'asociacion_session');
     if (asociaciones !== 0) {
       asociaciones.map(relation => {
@@ -146,29 +146,29 @@ var updateRelation = async function(body, idSession) {
     }
     let asocToRemove = oldAsociacionesOnSession.filter(item => !newAsociacionesOnSession.includes(item));
     let asocToAdd = newAsociacionesOnSession.filter(item => !oldAsociacionesOnSession.includes(item));
-  
+
     console.log('newAsociacionesOnSession -> ', newAsociacionesOnSession)
     console.log('oldAsociacionesOnSession -> ', oldAsociacionesOnSession)
-  
+
     console.log('asocToRemove -> ', asocToRemove)
     console.log('asocToAdd -> ', asocToAdd)
 
     if (asocToRemove.length > 0) {
       for (let asociacion of asocToRemove) {
-        try{
+        try {
           //console.log('Borrando asociación -> ', asociacion)
           await extraService.delete(asociacion, 'asociacion_session');
-        } catch(error) {
+        } catch (error) {
           reject(error);
         }
       }
-    }  
+    }
     if (asocToAdd.length > 0) {
       for (let asociacion of asocToAdd) {
         console.log('asocToAdd -> ', asociacion);
-        try{
+        try {
           await createRelation(asociacion, idSession);
-        } catch(error) {
+        } catch (error) {
           reject(error);
         }
       }
